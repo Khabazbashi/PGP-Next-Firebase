84a4e9a8a30fee2dd3df09a3b1a60a326112360e
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-31 11:53:02 +0100

    Add methods in firebaserepo to create a new item or update an item on click

--- counterrepo.js
+++ counterrepo.js
@@ -1,9 +1,28 @@
 import { db } from "./firebase";
-import { getDocs, collection } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  updateDoc,
+  doc,
+  setDoc,
+  Timestamp,
+} from "firebase/firestore";
 
 export const counterRepo = {
+  handleClick,
   getCounts,
 };
+
+async function createQuote(id, count) {
+  try {
+    const docRef = await setDoc(doc(db, "quotes", id), {
+      count: count,
+      createdAt: Timestamp.fromDate(new Date()),
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
 
 async function getCounts() {
   let data = [];
@@ -12,4 +31,24 @@ async function getCounts() {
     data.push({ id: doc.id, data: doc.data() });
   });
   return data;
+}
+
+async function updateQuote(id, newCount) {
+  try {
+    const docRef = doc(db, "quotes", id);
+
+    await updateDoc(docRef, {
+      count: newCount,
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+async function handleClick(id, count) {
+  if (count === 0) {
+    createQuote(id, count + 1);
+  } else {
+    updateQuote(id, count + 1);
+  }
 }

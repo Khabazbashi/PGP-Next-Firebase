a8a5495b4c5c35dfe67283a315d426eeeb01bbd8
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-31 11:46:31 +0100

    Match countdata in Quotes component with quotedata by id and pass down to Quote component

--- pages/quotes.js
+++ pages/quotes.js
@@ -1,11 +1,19 @@
 import Quote from "./quote";
+import { useEffect } from "react";
 
-const Quotes = ({ quotes }) => {
-  console.log(quotes);
+const Quotes = ({ quotes, counts }) => {
+  useEffect(() => {}, [counts]);
+
   return (
     <>
       {quotes.map((quote, index) => {
-        return <Quote quote={quote} key={index} />;
+        return (
+          <Quote
+            quote={quote}
+            key={index}
+            count={counts.filter((x) => x.id === quote.id)}
+          />
+        );
       })}
     </>
   );
